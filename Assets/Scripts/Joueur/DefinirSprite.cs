60058b2e2313b2fccb66d577b5c8a14402b443ae
GouVsky <greg6-marco@hotmail.fr>
2018-01-07 18:55:08 +0100

    Ajout d'une animation de vol.

---
+++ Assets/Scripts/Joueur/DefinirSprite.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefinirSprite : MonoBehaviour
+{
+	private SpriteRenderer __visuel;
+
+
+	void Start ()
+	{
+		__visuel = GetComponent <SpriteRenderer> ();
+	}
+	
+	void Update ()
+	{
+		
+	}
+
+	public void DefinirVisuel(Sprite sprite)
+	{
+		__visuel.sprite = sprite;
+	}
+}
--- Assets/Scripts/Joueur/Mouvements.cs
+++ Assets/Scripts/Joueur/Mouvements.cs
@@ -6,19 +6,35 @@ public class Mouvements : MonoBehaviour
 {
 	public float _puissance_saut;
 
+
 	public Rigidbody __rigid_body;
+
+	public DefinirSprite __sprite;
+
+	public Sprite __vol,
+				  __mannequin_challenge;
+
 
 
 	void Start()
 	{
 		__rigid_body = GetComponent <Rigidbody> ();
+
+		__sprite.DefinirVisuel (__mannequin_challenge);
 	}
 	
 	void Update()
 	{
-		if (Input.GetKey(KeyCode.Space))
+		if (Input.GetKey (KeyCode.Space))
 		{
-			__rigid_body.velocity = Vector3.up * _puissance_saut;//.AddForce (Vector3.up * _puissance_saut);
+			__rigid_body.velocity = Vector3.up * _puissance_saut;
+
+			__sprite.DefinirVisuel (__vol);
+		} 
+
+		else
+		{
+			__sprite.DefinirVisuel (__mannequin_challenge);
 		}
 	}
 }
