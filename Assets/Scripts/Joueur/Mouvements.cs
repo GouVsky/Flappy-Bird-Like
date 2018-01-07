e72fb7756e3e014a27280ab8dd826c613adf100b
GouVsky <greg6-marco@hotmail.fr>
2018-01-07 17:02:03 +0100

    Création du joueur + création du script de saut.

---
+++ Assets/Scripts/Joueur/Mouvements.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mouvements : MonoBehaviour
+{
+	public float _puissance_saut;
+
+	public Rigidbody __rigid_body;
+
+
+	void Start()
+	{
+		__rigid_body = GetComponent <Rigidbody> ();
+	}
+	
+	void Update()
+	{
+		if (Input.GetKey(KeyCode.Space))
+		{
+			__rigid_body.velocity = Vector3.up * _puissance_saut;//.AddForce (Vector3.up * _puissance_saut);
+		}
+	}
+}
