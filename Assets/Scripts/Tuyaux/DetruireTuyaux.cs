2ec5970b01ec45fd21a986812b4b314c517d7e48
GouVsky <greg6-marco@hotmail.fr>
2018-01-09 17:47:13 +0100

    Suppression des tuyaux lorsqu'ils se trouvent en dehors du champ de la caméra.

---
+++ Assets/Scripts/Tuyaux/DetruireTuyaux.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetruireTuyaux : MonoBehaviour
+{
+	void Start ()
+	{
+		
+	}
+	
+	void Update ()
+	{
+		
+	}
+
+	// Détruit le tuyau lorsqu'il n'est plus dans le champ de la caméra.
+
+	void OnBecameInvisible()
+	{
+		Destroy (gameObject);
+	}
+}
